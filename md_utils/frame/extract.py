a171aed089506b1c8bfc40aa802eb6a77d514a0e
OOLebedenko <oolebedenko@gmail.com>
2022-03-09 13:39:21 +0300

    refractor interface of function from frame to pdb file

--- md_utils/frame/extract.py
+++ md_utils/frame/extract.py
@@ -1,23 +1,5 @@
-from pyxmolpp2 import Molecule, Residue, AngleValue, ResidueSelection
+from pyxmolpp2 import Molecule, ResidueSelection
 from typing import List
-
-
-def extract_dihedral_angle_phi(residue: Residue) -> AngleValue:
-    """
-    see https://proteopedia.org/wiki/index.php/Dihedral/Dihedral_angles_in_proteins
-    :param residue:
-    :return: dihedral angle phi
-    """
-    ...
-
-
-def extract_dihedral_angle_psi(residue: Residue) -> AngleValue:
-    """
-    see https://proteopedia.org/wiki/index.php/Dihedral/Dihedral_angles_in_proteins
-    :param residue:
-    :return: dihedral angle psi
-    """
-    ...
 
 
 def extract_residues_on_interface(partner_A: Molecule,
@@ -37,18 +19,10 @@ def extract_residues_on_interface(partner_A: Molecule,
 if __name__ == "__main__":
     from pyxmolpp2 import PdbFile, mName
 
-    path_to_pdb = "path to your pdb"
+    path_to_pdb = "7jzu.pdb"
     frame = PdbFile(path_to_pdb).frames()[0]
 
-    phi_angles = []
-    psi_angles = []
-    omega_angles = []
-
-    for residue in frame.residues:
-        phi_angles.append(extract_dihedral_angle_phi(residue))
-        psi_angles.append(extract_dihedral_angle_psi(residue))
-
     [interface_residues_partner_A, interface_residues_partner_B] = extract_residues_on_interface(
-                                                                    partner_A=frame.molecules.filter(mName == "A"),
-                                                                    partner_B=frame.molecules.filter(mName == "B"),
-                                                                    cutoff=5)
+        partner_A=frame.molecules.filter(mName == "A"),
+        partner_B=frame.molecules.filter(mName == "B"),
+        cutoff=5)
--- md_utils/frame/select.py
+++ md_utils/frame/select.py
@@ -1,34 +1,38 @@
-from typing import Dict
-from pyxmolpp2 import Frame, AtomSelection
+from typing import List
+from pyxmolpp2 import AtomSelection
 
 
-def get_ss_residies(path_to_pdb: str) -> Dict[str, str]:
+def get_sec_str_residue_ids(path_to_pdb: str,
+                            molname: str
+                            ) -> List[int]:
     """
     to predict the secondary structure residues use DSSP https://biopython.org/docs/1.76/api/Bio.PDB.DSSP.html
-    :param path_to_pdb
-    :return: ss_rids_rnames_dict: key - residue id, value - type of secondary structure in DSSP notation
-                         helix - H (Alpha helix (4-12), G (3-10 helix) and I (Pi helix),
-                         strand E (Strand) and B (Isolated beta-bridge residue),
-                         loop - S (Bend), T (Turn)
-                         None (-)
+    :param path_to_pdb:
+    :param molname:
+    :return: list of residue id, which belongs to secondary-structure region
+             helix - H (Alpha helix (4-12), G (3-10 helix) and I (Pi helix)
+             or
+             strand E (Strand) and B (Isolated beta-bridge residue),
     """
     ...
 
 
-def select_ss_ca_atoms(frame: Frame,
-                       ) -> AtomSelection:
+def select_sec_str_ca_atoms(path_to_pdb: str,
+                            molname: str,
+                            ) -> AtomSelection:
     """
+    :param: frame: frame in MD trajectory
     :param path_to_pdb:
-    :param molnames:
+    :param molname: name of molecules.
+                    For example, reference structure is complex so that it contains two molecules of interacting partner
     :return: CA atoms belongs to secondary structured regions
     """
+    sec_str_rids = get_sec_str_residue_ids(path_to_pdb=path_to_pdb,
+                                           molname=molname)
     ...
 
 
 if __name__ == "__main__":
-    from pyxmolpp2 import PdbFile
+    path_to_pdb = "7jzu.pdb"
 
-    path_to_pdb = "path to your pdb"
-    frame = PdbFile(path_to_pdb).frames()[0]
-
-    ss_ca_atoms = select_ss_ca_atoms(frame)
+    ss_ca_atoms = select_sec_str_ca_atoms(path_to_pdb, molname="A")
