e2fc7fb4a6f28607350b06c922cd0603f91b72ac
OOLebedenko <oolebedenko@gmail.com>
2022-03-13 10:59:36 +0000

    test github actions

--- md_utils/frame/convert.py
+++ md_utils/frame/convert.py
@@ -6,9 +6,23 @@ def pdb_to_fasta(frame: Frame) -> None:
     writes one or more sequences from pdb file into a file in FASTA format
     :param path_to_pdb_file:
     :return: file in FASTA format
-    """
-    ...
+    """  
+    aa_code = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
+     'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N', 
+     'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
+     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+    
+    output = []
+    for i in range(len(frame.molecules)):
+        seq = []
+        for mol in frame.molecules[i].residues:
+            if mol.name in aa_code:
+                seq.append(aa_code[mol.name])
 
+        output.append('>' + str(frame.molecules[i].name))
+        output.append(str(''.join(seq)))
+    
+    print(*output, sep = '\n')
 
 if __name__ == "__main__":
     from pyxmolpp2 import PdbFile
