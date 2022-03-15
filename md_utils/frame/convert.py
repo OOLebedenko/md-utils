0e865f462b56a07ec07a441ca529b87a0eb36331
OOLebedenko <oolebedenko@gmail.com>
2022-03-15 06:40:23 +0000

    test actions

--- md_utils/frame/convert.py
+++ md_utils/frame/convert.py
@@ -10,7 +10,7 @@ def pdb_to_fasta(frame: Frame) -> None:
     aa_code = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
      'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N', 
      'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W', 
-     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+     'ALN': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
     
     output = []
     for i in range(len(frame.molecules)):
