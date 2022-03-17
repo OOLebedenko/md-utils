55f4a596264eaf250c54ec728b5ae40dad930706
Aleks Kovalenko <ikovalenko239@gmail.com>
2022-03-17 20:39:12 +0600

    edited extract.py to correct id-index relation

--- md_utils/frame/extract.py
+++ md_utils/frame/extract.py
@@ -1,7 +1,5 @@
-from pyxmolpp2 import Molecule, Residue, AtomSelection, ResidueSelection, aId
-from scipy.spatial import cKDTree # to compute distacnes
-from typing import List
-import numpy as np
+from pyxmolpp2 import Molecule, aId
+from scipy.spatial import cKDTree  # to compute distacnes
 
 
 def extract_residues_on_interface(partner_A: Molecule,
@@ -27,14 +25,15 @@ def extract_residues_on_interface(partner_A: Molecule,
     a_index, b_index = dist.nonzero()
 
     # get corresponding residues
-    first_a_index = partner_A.atoms[0].index # to correct indexes from matrix
-    resdue_selection_A = partner_A.atoms.filter(aId.is_in(set(a_index + first_a_index))).residues
-    
-    first_b_index = partner_B.atoms[0].index # to correct indexes from matrix
-    resdue_selection_B = partner_B.atoms.filter(aId.is_in(set(b_index + first_b_index))).residues
+    first_a_id = partner_A.atoms[0].id  # to correct indexes from matrix
+    # atom_index + first_atom_index + (first_atom_id - first_atom_index) = atom_index + first_atom_id
+    resdue_selection_A = partner_A.atoms.filter(aId.is_in(set(a_index + first_a_id))).residues
+
+    first_b_id = partner_B.atoms[0].id  # to correct indexes from matrix
+    resdue_selection_B = partner_B.atoms.filter(aId.is_in(set(b_index + first_b_id))).residues
 
     return [resdue_selection_A, resdue_selection_B]
-    
+
 
 if __name__ == "__main__":
     from pyxmolpp2 import PdbFile, mName
@@ -43,6 +42,6 @@ if __name__ == "__main__":
     frame = PdbFile(path_to_pdb).frames()[0]
 
     [interface_residues_partner_A, interface_residues_partner_B] = extract_residues_on_interface(
-                                                                    partner_A=frame.molecules.filter(mName == "A"),
-                                                                    partner_B=frame.molecules.filter(mName == "B"),
-                                                                    cutoff=5)
+        partner_A=frame.molecules.filter(mName == "A"),
+        partner_B=frame.molecules.filter(mName == "B"),
+        cutoff=5)
